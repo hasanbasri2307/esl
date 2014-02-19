3b1c09a561bf432009b657be127a1dabf3b8d325
Hasan Basri <hasanbasri2307@gmail.com>
2014-02-19 14:27:55 +0700

    test2

--- themes/ace/views/consultant/client/index.php
+++ themes/ace/views/consultant/client/index.php
@@ -10,7 +10,7 @@ $this->renderPartial('../menu',array(
 ?>
 
 <div class="page-header position-relative">
-    <h1>            Clients 
+    <h1>            Clients hahahahaha
             <small>
                     <i class="icon-double-angle-right"></i>
                    
