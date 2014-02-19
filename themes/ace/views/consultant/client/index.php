8bb23399b2dedd2977117e3a8fe169919218f92c
Hasan Basri <hasanbasri2307@gmail.com>
2014-02-19 14:17:06 +0700

    coba1

--- themes/ace/views/consultant/client/index.php
+++ themes/ace/views/consultant/client/index.php
@@ -10,7 +10,7 @@ $this->renderPartial('../menu',array(
 ?>
 
 <div class="page-header position-relative">
-    <h1>            Clients hasan
+    <h1>            Clients hasan basri
             <small>
                     <i class="icon-double-angle-right"></i>
                    
